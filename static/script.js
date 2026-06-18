cdb58755c31ecd0e55e1ddade01e84890fd0811d
Sahil-Dora59 <232745739+Sahil-Dora59@users.noreply.github.com>
2026-06-18 19:09:33 +0530

    Add chatbot JavaScript functionality

--- static/script.js
+++ static/script.js
@@ -1,1 +1,20 @@
+function sendMessage() {
 
+    let input = document.getElementById("user-input");
+
+    let chatBox = document.getElementById("chat-box");
+
+    let message = input.value.trim();
+
+    if (message === "") {
+        return;
+    }
+
+    chatBox.innerHTML += "<p><b>You:</b> " + message + "</p>";
+
+    chatBox.innerHTML += "<p><b>Chatbot:</b> Thank you for your question. AI response will be added soon.</p>";
+
+    input.value = "";
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
